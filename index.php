eca81c846e7cc12aeb4b89c98fad937c56db9be5
AstuOswal! <37967143+astuoswal@users.noreply.github.com>
2018-11-28 11:10:35 -0400

    Se agrega codigo al script para que funcione la condicional

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
+require_once 'module/autoload_class.php';
+require_once 'module/validate_session.php';
 
-if (!isset($_SESSION["ID"])) {
+if (!(isset($_SESSION["id"]))) {
     header("location: template/views/login.php");
 } else {
     header("location: template/views/home.php");
